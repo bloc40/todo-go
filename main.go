f9a3e5da86a4a301a349ec12f52c8c731559f481
Jamal El Milahi <jamal@elmilahi.com>
2016-03-07 18:28:39 -0800

    Edit an item in the list

--- main.go
+++ main.go
@@ -12,47 +12,52 @@ type Item struct {
 	title string
 }
 
-func Display(list []Item) {
-	fmt.Println("\n---------- You list is ----------")
-	for index, item := range list {
-		fmt.Println(index, ": ", item.title)
-	}
-	fmt.Println("---------------------------------\n")
-}
-
 var	reader = bufio.NewReader(os.Stdin)
 
 func main() {
 	list := []Item{}
 
-	var choice float64
+	var choice int64
 
-	for choice != 4 {
+	for choice != 5 {
 		choice = getChoice()
 
 		switch choice {
 		case 1:
 			Display(list)
 		case 2:
-			fmt.Print("Enter item: ")
+			fmt.Print("Enter item to add: ")
 			input, err := reader.ReadString('\n')
 			checkError(err)
 
 			item := Item{strings.TrimSpace(input)}
 			list = append(list, item)
-
-			// Display(list)
 		case 3:
+			Display(list)
+			fmt.Print("Enter the item number to edit: ")
+			input, err := reader.ReadString('\n')
+			checkError(err)
+
+			itemNumber, err := strconv.ParseInt(strings.TrimSpace(input), 10, 64)
+			checkError(err)
+
+			fmt.Print("Edit item number ", itemNumber, ": ")
+			input, err = reader.ReadString('\n')
+			checkError(err)
+
+			item := Item{strings.TrimSpace(input)}
+			list[itemNumber] = item
+		case 4:
 			Display(list)
 			fmt.Print("Enter the item number to delete: ")
 			input, err := reader.ReadString('\n')
 			checkError(err)
 
-			itemNumber,err := strconv.ParseInt(strings.TrimSpace(input), 10, 64)
+			itemNumber, err := strconv.ParseInt(strings.TrimSpace(input), 10, 64)
 			checkError(err)
 
 			list = append(list[:itemNumber], list[itemNumber+1:]...)
-		case 4:
+		case 5:
 			fmt.Println("\nBye bye")
 		default:
 			fmt.Println("\nBad selection. Try again!")
@@ -60,17 +65,26 @@ func main() {
 	}
 }
 
-func getChoice() float64 {
-	fmt.Println("What to do?")
+func Display(list []Item) {
+	fmt.Println("\n---------- You list is ----------")
+	for index, item := range list {
+		fmt.Println(index, ": ", item.title)
+	}
+	fmt.Println("---------------------------------")
+}
+
+func getChoice() int64 {
+	fmt.Println("\nWhat to do?")
 	fmt.Println("1. Show List")
 	fmt.Println("2. Add an item")
-	fmt.Println("3. Delete an item")
-	fmt.Println("4. Quit")
+	fmt.Println("3. Edit an item")
+	fmt.Println("4. Delete an item")
+	fmt.Println("5. Quit")
 	fmt.Print("> ")
 
 	input, err := reader.ReadString('\n')
 	checkError(err)
-	choice, err := strconv.ParseFloat(strings.TrimSpace(input), 64)
+	choice, err := strconv.ParseInt(strings.TrimSpace(input), 10, 64)
 	checkError(err)
 	return choice
 }
